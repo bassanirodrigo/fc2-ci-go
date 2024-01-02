d510e2de137338defddabc7e30df4c058dbaeb4f
bassanirodrigo <bassani.rodrigo@hotmail.com>
2024-01-02 17:13:09 -0300

    feat(Alterado a classe de test): :zap: Alteração do código para incluir o teste com 20

--- math_test.go
+++ math_test.go
@@ -4,9 +4,9 @@ import "testing"
 
 func TestSoma(t *testing.T) {
 
-	total := soma(15, 15)
+	total := soma(10, 10)
 
-	if total != 30 {
-		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 30)
+	if total != 20 {
+		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 20)
 	}
 }
